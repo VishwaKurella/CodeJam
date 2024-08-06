e6999088851a40fddfe02209248fbac915bb875e
Vishwanath Kurella <vishwa.kurella@icloud.com>
2024-08-07 00:12:00 +0530

    first commit

---
+++ d1000000.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class d1000000 {
+    public static void main(String[] args){
+        Scanner S = new Scanner(System.in);
+        int T = S.nextInt();
+        while(T-- >0){
+            int N = S.nextInt();
+            PriorityQueue<Integer> queue = new PriorityQueue<>(N);
+            int result = 0;
+            for(int i=0;i<N;i++){
+                queue.offer(S.nextInt());
+            }
+            while(!queue.isEmpty()){
+                if(queue.poll() >= result+1) result++;
+            }
+            System.out.println("Case #:"+result);
+        }
+    }
+}
